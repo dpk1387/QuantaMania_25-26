826dbb7eb04578200f6e75d5305fb4bc12429185
BenGithub3 <benjaminhahome@gmail.com>
2026-01-16 18:57:59 -0800

    new classifier pose, newShootX, newShootY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueGateNear_Version2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueGateNear_Version2.java
@@ -346,11 +346,11 @@ public class BlueGateNear_Version2 extends LinearOpMode {
         runtime.reset();
         telemetryThread.start();
         double shootX = -29, shootY = -29; //-29, -29 //-27.5, -27.5 //-28, 28 //30, 30
-        double newShootX =-25, newShootY = -25; //-24, -24 //-21, 21 //-16, 16 //-13, 13
+        double newShootX =-26, newShootY = -26; //-24, -24 //-21, 21 //-16, 16 //-13, 13
         Pose2d shootPose = new Pose2d(shootX, shootY, Math.toRadians(-135));
         Pose2d newShootPose = new Pose2d(newShootX, newShootY, Math.toRadians(-135));
 
-        Pose2d classifierPose = new Pose2d(7.5+0.2, -64-1,  Math.toRadians(240)); //120
+        Pose2d classifierPose = new Pose2d(7.5+0.2, -64-3,  Math.toRadians(240)); //120
 
         while (opModeIsActive()){
             try {
